82800ed2a61b3df50bb0da47425f06bf97e26aa3
Your Name <you@example.com>
2020-06-10 01:20:38 -0400

    good night

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,12 +2,24 @@ import React from "react";
 import "./Projects.css";
 import ReactProjects from "./Stacks/React/ReactProjects";
 import AngularProjects from "./Stacks/Angular/AngularProjects";
+import ReactNativeProject from "./Stacks/React_Native/ReactNative";
+import JavaProject from "./Stacks/Java/JavaProject";
+import JavascriptProject from "./Stacks/Javascript/JavascriptProject";
+import NodejsProject from "./Stacks/Nodejs/NodejsProject";
+import PythonProject from "./Stacks/Python/PythonProject";
+import AndroidProject from "./Stacks/Android/AndroidProject";
 
 const Projects = (props) => {
   return (
     <div className="Container">
       {props.status === "" || (props.status === "React" && <ReactProjects />)}
       {props.status === "Angular" && <AngularProjects />}
+      {props.status === "React_Native" && <ReactNativeProject />}
+      {props.status === "Java" && <JavaProject />}
+      {props.status === "Javascript" && <JavascriptProject />}
+      {props.status === "Node.js" && <NodejsProject />}
+      {props.status === "Python" && <PythonProject />}
+      {props.status === "Android" && <AndroidProject />}
     </div>
   );
 };

728d3891432b48076379e7824d7bee369e9a4bba
Your Name <you@example.com>
2020-06-16 22:24:43 -0400

    Updates

---
+++ precache-manifest.251d40bdc8585c7f740102c68f460b23.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a8f05792848066d04610dcdfb413a22",
+    "url": "/React_PersonalProjectsListing/index.html"
+  },
+  {
+    "revision": "a78d02410b3231aa0306",
+    "url": "/React_PersonalProjectsListing/static/css/2.ecf09bb4.chunk.css"
+  },
+  {
+    "revision": "c8eae5b6db28460745c7",
+    "url": "/React_PersonalProjectsListing/static/css/main.5474ac1a.chunk.css"
+  },
+  {
+    "revision": "a78d02410b3231aa0306",
+    "url": "/React_PersonalProjectsListing/static/js/2.cde44ad2.chunk.js"
+  },
+  {
+    "revision": "0138302c61f332a35160a01dfe27bb4a",
+    "url": "/React_PersonalProjectsListing/static/js/2.cde44ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8eae5b6db28460745c7",
+    "url": "/React_PersonalProjectsListing/static/js/main.8702d2d4.chunk.js"
+  },
+  {
+    "revision": "d97fe982173594762d3f",
+    "url": "/React_PersonalProjectsListing/static/js/runtime-main.57d7c1ee.js"
+  },
+  {
+    "revision": "b0e2dcda8f6cb3bab868f87bbbc10b81",
+    "url": "/React_PersonalProjectsListing/static/media/burgerimg.b0e2dcda.PNG"
+  },
+  {
+    "revision": "fa59f98ce0c1d038150d9be92ae40fd0",
+    "url": "/React_PersonalProjectsListing/static/media/eclipse-1492818.fa59f98c.jpg"
+  },
+  {
+    "revision": "131245ac3ea0169677095f6f1b3ead3e",
+    "url": "/React_PersonalProjectsListing/static/media/lockimg.131245ac.PNG"
+  },
+  {
+    "revision": "ce100dff722c9e139b11cd1dc8f3018a",
+    "url": "/React_PersonalProjectsListing/static/media/mernimg.ce100dff.PNG"
+  },
+  {
+    "revision": "593b90f1a0f2f91c528a85cb8ac5db66",
+    "url": "/React_PersonalProjectsListing/static/media/por2img.593b90f1.PNG"
+  },
+  {
+    "revision": "5be29189f47769993c07ae0ca2bbf3cf",
+    "url": "/React_PersonalProjectsListing/static/media/porfolioimg.5be29189.PNG"
+  },
+  {
+    "revision": "c50b491a32fa0b36a1086b89d4f65b7c",
+    "url": "/React_PersonalProjectsListing/static/media/projectListimg.c50b491a.PNG"
+  },
+  {
+    "revision": "018960791e113e0edc6cc33d30cd2997",
+    "url": "/React_PersonalProjectsListing/static/media/vidlyimg.01896079.PNG"
+  }
+]);
